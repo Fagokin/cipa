96b007154ec13f948f58898f0051eed3e615e743
Henrique Felix <149063665+Fagokin@users.noreply.github.com>
2026-01-07 20:42:38 -0300

    Retirada de comentarios no Usuario.php

--- models/Usuario.php
+++ models/Usuario.php
@@ -26,11 +26,11 @@
             $_admUsuario,
             $_matriculaUsuario,
             $_cpfUsuario,
-            $_telefoneUsuario = '',  //não obrigado
-            $_emailUsuario = '', //não obrigado
-            $_codigoVotoUsuario = '', //não obrigado
-            $_idUsuario = 0, //não obrgaigado
-            $_ultimoAcessoUsuario = '', //não obrigado
+            $_telefoneUsuario = '',  
+            $_emailUsuario = '', 
+            $_codigoVotoUsuario = '', 
+            $_idUsuario = 0, 
+            $_ultimoAcessoUsuario = '', 
 
 
         ) {
@@ -50,37 +50,21 @@
             $this->ultimoAcessoUsuario = $_ultimoAcessoUsuario;
         }
 
-        /**
-         * Get the value of idUsuario
-         */ 
         public function getIdUsuario()
         {
                 return $this->idUsuario;
         }
 
-        /**
-         * Set the value of idUsuario
-         *
-         * @return  self
-         */ 
         public function setIdUsuario($idUsuario){
                 $this->idUsuario = $idUsuario;
                 return $this;
         }
 
-        /**
-         * Get the value of sobrenomeUsuario
-         */ 
         public function getSobrenomeUsuario()
         {
                 return $this->sobrenomeUsuario;
         }
 
-        /**
-         * Set the value of sobrenomeUsuario
-         *
-         * @return  self
-         */ 
         public function setSobrenomeUsuario($sobrenomeUsuario)
         {
                 $this->sobrenomeUsuario = $sobrenomeUsuario;
@@ -88,38 +72,22 @@
                 return $this;
         }
 
-        /**
-         * Get the value of nomeUsuario
-         */ 
         public function getNomeUsuario()
         {
                 return $this->nomeUsuario;
         }
 
-        /**
-         * Set the value of nomeUsuario
-         *
-         * @return  self
-         */ 
         public function setNomeUsuario($nomeUsuario)
         {
                 $this->nomeUsuario = $nomeUsuario;
                 return $this;
         }
 
-        /**
-         * Get the value of emailUsuario
-         */ 
         public function getEmailUsuario()
         {
                 return $this->emailUsuario;
         }
 
-        /**
-         * Set the value of emailUsuario
-         *
-         * @return  self
-         */ 
         public function setEmailUsuario($emailUsuario)
         {
                 $this->emailUsuario = $emailUsuario;
@@ -127,19 +95,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of senhaUsuario
-         */ 
         public function getSenhaUsuario()
         {
                 return $this->senhaUsuario;
         }
 
-        /**
-         * Set the value of senhaUsuario
-         *
-         * @return  self
-         */ 
         public function setSenhaUsuario($senhaUsuario)
         {
                 $this->senhaUsuario = $senhaUsuario;
@@ -147,19 +107,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of datadeNascimentoUuario
-         */ 
         public function getDatadeNascimentoUuario()
         {
                 return $this->datadeNascimentoUuario;
         }
-
-        /**
-         * Set the value of datadeNascimentoUuario
-         *
-         * @return  self
-         */ 
+-
         public function setDatadeNascimentoUuario($datadeNascimentoUuario)
         {
                 $this->datadeNascimentoUuario = $datadeNascimentoUuario;
@@ -167,19 +119,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of dataContratacaoUsuario
-         */ 
         public function getDataContratacaoUsuario()
         {
                 return $this->dataContratacaoUsuario;
         }
 
-        /**
-         * Set the value of dataContratacaoUsuario
-         *
-         * @return  self
-         */ 
         public function setDataContratacaoUsuario($dataContratacaoUsuario)
         {
                 $this->dataContratacaoUsuario = $dataContratacaoUsuario;
@@ -187,19 +131,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of ativoUsuario
-         */ 
         public function getAtivoUsuario()
         {
                 return $this->ativoUsuario;
         }
 
-        /**
-         * Set the value of ativoUsuario
-         *
-         * @return  self
-         */ 
         public function setAtivoUsuario($ativoUsuario)
         {
                 $this->ativoUsuario = $ativoUsuario;
@@ -207,19 +143,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of admUsuario
-         */ 
         public function getAdmUsuario()
         {
                 return $this->admUsuario;
         }
 
-        /**
-         * Set the value of admUsuario
-         *
-         * @return  self
-         */ 
         public function setAdmUsuario($admUsuario)
         {
                 $this->admUsuario = $admUsuario;
@@ -227,19 +155,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of matriculaUsuario
-         */ 
         public function getMatriculaUsuario()
         {
                 return $this->matriculaUsuario;
         }
 
-        /**
-         * Set the value of matriculaUsuario
-         *
-         * @return  self
-         */ 
         public function setMatriculaUsuario($matriculaUsuario)
         {
                 $this->matriculaUsuario = $matriculaUsuario;
@@ -247,19 +167,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of cpfUsuario
-         */ 
         public function getCpfUsuario()
         {
                 return $this->cpfUsuario;
         }
 
-        /**
-         * Set the value of cpfUsuario
-         *
-         * @return  self
-         */ 
         public function setCpfUsuario($cpfUsuario)
         {
                 $this->cpfUsuario = $cpfUsuario;
@@ -267,19 +179,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of telefoneUsuario
-         */ 
         public function getTelefoneUsuario()
         {
                 return $this->telefoneUsuario;
         }
 
-        /**
-         * Set the value of telefoneUsuario
-         *
-         * @return  self
-         */ 
         public function setTelefoneUsuario($telefoneUsuario)
         {
                 $this->telefoneUsuario = $telefoneUsuario;
@@ -287,19 +191,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of codigoVotoUsuario
-         */ 
         public function getCodigoVotoUsuario()
         {
                 return $this->codigoVotoUsuario;
         }
 
-        /**
-         * Set the value of codigoVotoUsuario
-         *
-         * @return  self
-         */ 
         public function setCodigoVotoUsuario($codigoVotoUsuario)
         {
                 $this->codigoVotoUsuario = $codigoVotoUsuario;
@@ -307,19 +203,11 @@
                 return $this;
         }
 
-        /**
-         * Get the value of ultimoAcessoUsuario
-         */ 
         public function getUltimoAcessoUsuario()
         {
                 return $this->ultimoAcessoUsuario;
         }
 
-        /**
-         * Set the value of ultimoAcessoUsuario
-         *
-         * @return  self
-         */ 
         public function setUltimoAcessoUsuario($ultimoAcessoUsuario)
         {
                 $this->ultimoAcessoUsuario = $ultimoAcessoUsuario;
@@ -327,5 +215,6 @@
                 return $this;
         }
     }
+
 
 ?>
